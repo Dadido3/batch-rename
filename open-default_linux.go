e528fc41d6f1f569a96aa3a54d6ec5e42f32fb18
David Vogel <Dadido3@aol.com>
2021-10-12 17:37:51 +0200

    Fix tests under linux

---
+++ open-default_linux.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2021 David Vogel
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package main
+
+import "os/exec"
+
+func openWithDefault(filePath string) error {
+	// TODO: Add code to open file with default program on linux
+}
